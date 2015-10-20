70f429d4dfff6f3f5f512eb3763d44ae4df3a220
Andrew Porter <haporter@me.com>
2015-10-20 16:14:02 -0600

    Implement delegarte and datasource

---
+++ Timer/TimerViewController.swift
@@ -0,0 +1,63 @@
+//
+//  TimerViewController.swift
+//  Timer
+//
+//  Created by Andrew Porter on 10/20/15.
+//  Copyright © 2015 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class TimerViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var hourPickerView: UIPickerView!
+    @IBOutlet weak var minutePickerView: UIPickerView!
+    @IBOutlet weak var pauseButton: UIButton!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: UIPickerView Data Source
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView == hourPickerView {
+            return 24
+        } else {
+        return 60
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func startButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func pauseButtonPressed(sender: UIButton) {
+    }
+    
+
+}
